4a88612563145300e89a90aebdda21a482e5d34d
stranger-johnny <kurokuro1358@gmail.com>
2025-02-02 19:53:57 +0900

    fix

--- dist/index.js
+++ dist/index.js
@@ -958,12 +958,24 @@ class PullsClient {
         this.gitHubClient = gitHubClient;
     }
     async collect() {
-        return await this.gitHubClient.octokit.paginate(this.gitHubClient.octokit.rest.pulls.list, {
-            owner: this.gitHubClient.owner,
-            repo: this.gitHubClient.repo,
-            state: 'all',
-            per_page: 100,
-        });
+        const pulls = [];
+        let page = 1;
+        while (true) {
+            const curratePagePulls = await this.gitHubClient.octokit.paginate(this.gitHubClient.octokit.rest.pulls.list, {
+                owner: this.gitHubClient.owner,
+                repo: this.gitHubClient.repo,
+                state: 'all',
+                per_page: 1,
+                page: page,
+            });
+            console.log('curratePagePulls:', curratePagePulls.map((pull) => pull.title));
+            if (curratePagePulls.length === 0) {
+                break;
+            }
+            pulls.push(...curratePagePulls);
+            page++;
+        }
+        return pulls;
     }
 }
 exports.PullsClient = PullsClient;
--- src/pulls/pulls_client.ts
+++ src/pulls/pulls_client.ts
@@ -5,14 +5,33 @@ export class PullsClient {
   constructor(private gitHubClient: GitHubClient) {}
 
   public async collect(): Promise<Pull[]> {
-    return await this.gitHubClient.octokit.paginate(
-      this.gitHubClient.octokit.rest.pulls.list,
-      {
-        owner: this.gitHubClient.owner,
-        repo: this.gitHubClient.repo,
-        state: 'all',
-        per_page: 100,
+    const pulls: Pull[] = []
+
+    let page = 1
+    while (true) {
+      const curratePagePulls = await this.gitHubClient.octokit.paginate(
+        this.gitHubClient.octokit.rest.pulls.list,
+        {
+          owner: this.gitHubClient.owner,
+          repo: this.gitHubClient.repo,
+          state: 'all',
+          per_page: 1,
+          page: page,
+        }
+      )
+
+      console.log(
+        'curratePagePulls:',
+        curratePagePulls.map((pull) => pull.title)
+      )
+
+      if (curratePagePulls.length === 0) {
+        break
       }
-    )
+      pulls.push(...curratePagePulls)
+      page++
+    }
+
+    return pulls
   }
 }
