3d1f23ceafa85dea8f7709c8a914f0a92d10c162
stranger-johnny <kurokuro1358@gmail.com>
2025-02-02 16:43:29 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -18,33 +18,32 @@ async function getPrsAverageTime(owner: string, repo: string) {
     {
       owner,
       repo,
-      state: undefined,
       per_page: 100,
     }
   )
 
   console.log(prs)
 
-  const filteredPrs = prs.filter((pr) => {
-    const createdAt = new Date(pr.created_at)
-    return createdAt >= oneWeekAgo && pr.closed_at
-  })
+  // const filteredPrs = prs.filter((pr) => {
+  //   const createdAt = new Date(pr.created_at)
+  //   return createdAt >= oneWeekAgo && pr.closed_at
+  // })
 
-  if (filteredPrs.length === 0) {
-    console.log('No PRs closed in the last week.')
-    return
-  }
+  // if (filteredPrs.length === 0) {
+  //   console.log('No PRs closed in the last week.')
+  //   return
+  // }
 
-  const totalTime = filteredPrs.reduce((acc, pr) => {
-    const createdAt = new Date(pr.created_at).getTime()
-    const closedAt = new Date(pr.closed_at!).getTime()
-    return acc + (closedAt - createdAt)
-  }, 0)
+  // const totalTime = filteredPrs.reduce((acc, pr) => {
+  //   const createdAt = new Date(pr.created_at).getTime()
+  //   const closedAt = new Date(pr.closed_at!).getTime()
+  //   return acc + (closedAt - createdAt)
+  // }, 0)
 
-  const averageTimeMs = totalTime / filteredPrs.length
-  const averageTimeHours = averageTimeMs / (1000 * 60 * 60)
+  // const averageTimeMs = totalTime / filteredPrs.length
+  // const averageTimeHours = averageTimeMs / (1000 * 60 * 60)
 
-  console.log(`Average PR open time: ${averageTimeHours.toFixed(2)} hours`)
+  // console.log(`Average PR open time: ${averageTimeHours.toFixed(2)} hours`)
 }
 
 getPrsAverageTime('stranger-johnny', 'activity-analyzer').catch(console.error)
