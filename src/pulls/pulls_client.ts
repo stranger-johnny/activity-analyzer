8acb60beca1e29079c007f2758044e1f23567d0c
stranger-johnny <kurokuro1358@gmail.com>
2025-02-02 20:02:11 +0900

    fix

--- src/pulls/pulls_client.ts
+++ src/pulls/pulls_client.ts
@@ -5,35 +5,14 @@ export class PullsClient {
   constructor(private gitHubClient: GitHubClient) {}
 
   public async collect(): Promise<Pull[]> {
-    const pulls: Pull[] = []
-
-    let page = 1
-    while (true) {
-      const curratePagePulls = await this.gitHubClient.octokit.paginate(
-        this.gitHubClient.octokit.rest.pulls.list,
-        {
-          owner: this.gitHubClient.owner,
-          repo: this.gitHubClient.repo,
-          state: 'all',
-          per_page: 2,
-          page: page,
-        }
-      )
-
-      console.log(
-        'page',
-        page,
-        'curratePagePulls:',
-        curratePagePulls.map((pull) => pull.title)
-      )
-
-      if (curratePagePulls.length === 0) {
-        break
+    return await this.gitHubClient.octokit.paginate(
+      this.gitHubClient.octokit.rest.pulls.list,
+      {
+        owner: this.gitHubClient.owner,
+        repo: this.gitHubClient.repo,
+        state: 'all',
+        per_page: 100,
       }
-      pulls.push(...curratePagePulls)
-      page++
-    }
-
-    return pulls
+    )
   }
 }
